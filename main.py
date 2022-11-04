3a83759e6dcc9cfda5a2a91d97e1d6908d6ee003
Matt-Healey023 <matthealey023@gmail.com>
2022-11-03 20:26:51 -0400

    Will target food now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import typing
+import math
 
 def info() -> typing.Dict:
     print("INFO")
@@ -79,15 +80,50 @@ def move(game_state: typing.Dict) -> typing.Dict:
         if isSafe:
             safe_moves.append(move)
 
-    if len(safe_moves) == 0:
-        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
-        return {"move": "down"}
+    # if len(safe_moves) == 0:
+    #     print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
+    #     return {"move": "down"}
 
-    # Choose a random move from the safe ones
-    next_move = random.choice(safe_moves)
+    # Get move to closest food
+    next_move = None
+    food = game_state['board']['food']
+    if len(food) > 0:
+        index = -1
+        close = 0
+        for i in range(len(food)):
+            dis = math.sqrt((head['x'] - food[i]['x']) ** 2 + (head['y'] - food[i]['y']) ** 2)
+            if index == -1 or dis < close:
+                index = i
+                close = dis
+        
+        pellet = food[index]
+        if abs(pellet['x'] - head['x']) > abs(pellet['y'] - head['y']):
+            if head['x'] - pellet['x'] < 0 and is_move_safe['right']:
+                next_move = 'right'
+            elif is_move_safe['left']:
+                next_move = 'left'
 
-    # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    # food = game_state['board']['food']
+            if next_move == None:
+                if head['y'] - pellet['y'] < 0 and is_move_safe['up']:
+                    next_move = 'up'
+                elif is_move_safe['down']:
+                    next_move = 'down'
+        else:
+            if head['y'] - pellet['y'] < 0 and is_move_safe['up']:
+                next_move = 'up'
+            elif is_move_safe['down']:
+                next_move = 'down'
+
+            if next_move == None:
+                if head['x'] - pellet['x'] < 0 and is_move_safe['right']:
+                    next_move = 'right'
+                elif is_move_safe['left']:
+                    next_move = 'left'
+
+        
+
+    if next_move == None:
+        next_move = random.choice(safe_moves)
 
     print(f"MOVE {game_state['turn']}: {next_move}")
     return {"move": next_move}
