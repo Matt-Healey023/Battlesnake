3874dc0fb6feee0df564ead5608fe7e30787d816
Matt-Healey023 <matthealey023@gmail.com>
2022-11-04 19:37:17 -0400

    Looking forward now too

--- move.py
+++ move.py
@@ -1,103 +1,3 @@
-# is_move_safe = {"up": True, "down": True, "left": True, "right": True}
-
-# # Body check
-# head = game_state['you']['head']
-# body = game_state['you']['body'][1:]
-
-# for i in range(len(body)):
-#     x = head['x'] - body[i]['x']
-#     y = head['y'] - body[i]['y']
-#     if y == 0:
-#         if x == 1:
-#             is_move_safe['left'] = False
-#         if x == -1:
-#             is_move_safe['right'] = False
-
-#     if x == 0:
-#         if y == 1:
-#             is_move_safe['down'] = False
-#         if y == -1:
-#             is_move_safe['up'] = False
-
-#     # print(f"({head['x']}, {head['y']}) - ({body[i]['x']}, {body[i]['y']}) = ({x}, {y})")
-
-# # Out-of-bounds
-# width = game_state['board']['width']
-# height = game_state['board']['height']
-
-# if head['x'] == 0:
-#     is_move_safe['left'] = False
-# elif head['x'] == width - 1:
-#     is_move_safe['right'] = False
-
-# if head['y'] == height - 1:
-#     is_move_safe['up'] = False
-# elif head['y'] == 0:
-#     is_move_safe['down'] = False
-
-# # Basic enemy avoidance
-# for snake in game_state['board']['snakes']:
-#     for i in range(len(snake['body'])):
-#         x = head['x'] - snake['body'][i]['x']
-#         y = head['y'] - snake['body'][i]['y']
-#         if y == 0:
-#             if x == 1:
-#                 is_move_safe['left'] = False
-#             if x == -1:
-#                 is_move_safe['right'] = False
-
-#         if x == 0:
-#             if y == 1:
-#                 is_move_safe['down'] = False
-#             if y == -1:
-#                 is_move_safe['up'] = False
-
-# # Are there any safe moves left?
-# safe_moves = []
-# for move, isSafe in is_move_safe.items():
-#     if isSafe:
-#         safe_moves.append(move)
-
-# # if len(safe_moves) == 0:
-# #     print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
-# #     return {"move": "down"}
-
-# # Get move to closest food
-# next_move = None
-# food = game_state['board']['food']
-# if len(food) > 0:
-#     index = -1
-#     close = 0
-#     for i in range(len(food)):
-#         dis = math.sqrt((head['x'] - food[i]['x']) ** 2 + (head['y'] - food[i]['y']) ** 2)
-#         if index == -1 or dis < close:
-#             index = i
-#             close = dis
-    
-#     pellet = food[index]
-#     if abs(pellet['x'] - head['x']) > abs(pellet['y'] - head['y']):
-#         if head['x'] - pellet['x'] < 0 and is_move_safe['right']:
-#             next_move = 'right'
-#         elif is_move_safe['left']:
-#             next_move = 'left'
-
-#         if next_move == None:
-#             if head['y'] - pellet['y'] < 0 and is_move_safe['up']:
-#                 next_move = 'up'
-#             elif is_move_safe['down']:
-#                 next_move = 'down'
-#     else:
-#         if head['y'] - pellet['y'] < 0 and is_move_safe['up']:
-#             next_move = 'up'
-#         elif is_move_safe['down']:
-#             next_move = 'down'
-
-#         if next_move == None:
-#             if head['x'] - pellet['x'] < 0 and is_move_safe['right']:
-#                 next_move = 'right'
-#             elif is_move_safe['left']:
-#                 next_move = 'left'
-
 import random
 
 def checkForHazards(game_state):
@@ -133,12 +33,14 @@ def checkForHazards(game_state):
     if head['y'] == h - 1: safe['up'] = False
     elif head['y'] == 0: safe['down'] = False
 
-    # Check for neaby heads
+    # Check for nearby heads
     for eHead in eHeads:
         x = eHead[0]['x'] - head['x']
         y = eHead[0]['y'] - head['y']
         # '>=' to play safe, '>' to be agressive and probably die
+        # Only be scared if you have a way out
         if eHead[1] >= size:
+            # Diagonal
             if x == -1:
                 if y == -1 and (safe['right'] or safe['up']):
                     safe['left'] = False
@@ -153,6 +55,14 @@ def checkForHazards(game_state):
                 if y == 1 and (safe['left'] or safe['down']):
                     safe['right'] = False
                     safe['up'] = False
+            # Horizontal
+            if y == 0:
+                if x == -2 and (safe['down'] or safe['up'] or safe['right']): safe['left'] = False
+                if x == 2 and (safe['down'] or safe['up'] or safe['left']): safe['right'] = False
+            # Vertical
+            if x == 0:
+                if y == -2 and (safe['right'] or safe['up'] or safe['left']): safe['down'] = False
+                if y == 2 and (safe['right'] or safe['down'] or safe['left']): safe['up'] = False
 
     return safe
 
